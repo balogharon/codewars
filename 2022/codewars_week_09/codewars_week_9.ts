aa1b9d9765f18415d94a9a9a7d1ae0cb1a48ad65
Aron Balogh <balogh.aron@gmail.com>
2022-05-25 19:29:45 +0200

    Continued

--- 2022/codewars_week_09/codewars_week_9.ts
+++ 2022/codewars_week_09/codewars_week_9.ts
@@ -9,4 +9,20 @@ const formatMoney = (amount: any) => `$${parseFloat(amount).toFixed(2)}`;
 Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 Return your answer as a number.
 */
-export const sumMix = (x: any[]) => x.reduce((a: any, b: any) => parseInt(a) + parseInt(b));
+const sumMix = (x: any[]): number => x.reduce((a: any, b: any) => parseInt(a) + parseInt(b));
+
+/* week 9 - task 003
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+*/
+const filter_list = (l: any[]) => l.filter((x: number | string) => typeof x !== 'string');
+
+/* week 9 - task 004
+You will be given a vector of strings.
+You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+The returned value must be a string, and have "***" between each of its letters.
+You should not remove or add elements from/to the array.
+*/
+export const twoSort = (s: string[]): string[] => {
+  console.log(s);
+  return s;
+};
